fa638331cc5335d69c661c20826c36c21c9e5340
jgwehr <dm.nota@gmail.com>
2023-07-12 12:09:14 -0500

    Pihole/Unbound configs

--- scripts/run-once.sh
+++ scripts/run-once.sh
@@ -7,6 +7,11 @@ cp $varMainDir/.env.template $varMainDir/.env
 
 
 ####################
+# adblock-and-dns
+####################
+# !!!! TO BE FINISHED!, follow README.md
+
+####################
 # infra-docker (required)
 ####################
 sh $varMainDir/services/infra-docker/run-once.sh
