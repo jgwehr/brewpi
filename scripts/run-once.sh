5797c35db60221511c2bb3a777b3c3e2128b500b
Joe <jgwehr@users.noreply.github.com>
2025-07-09 21:19:17 -0500

    Better Unbound Networking

--- scripts/run-once.sh
+++ scripts/run-once.sh
@@ -9,7 +9,18 @@ cp $varMainDir/.env.template $varMainDir/.env
 ####################
 # adblock-and-dns
 ####################
-# !!!! TO BE FINISHED!, follow README.md
+# Unbound
+sudo mkdir -p $varConfigDir/pihole-unbound/pihole/etc-pihole
+sudo mkdir -p $varConfigDir/pihole-unbound/pihole/etc-dnsmasq.d
+sudo mkdir -p $varConfigDir/pihole-unbound/unbound/etc-unbound
+sudo mkdir -p $varConfigDir/pihole-unbound/unbound/unbound.conf.d
+
+touch $varConfigDir/pihole-unbound/unbound/etc-unbound/unbound.log
+
+chown -R $USER:docker $varConfigDir/pihole-unbound
+chmod -R 755 $varConfigDir/pihole-unbound
+
+cp -rpi $varOptDir/services/adblock-and-dns/configtemplates/unbound/* $varConfigDir/pihole-unbound/unbound
 
 ####################
 # infra-docker (required)
